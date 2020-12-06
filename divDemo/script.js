f59304d297b8654921b9943e70e0386bd75edcf7
Preston Hager <preston@hagerfamily.com>
2020-12-05 21:25:13 -0800

    Created code animation.

--- divDemo/script.js
+++ divDemo/script.js
@@ -1,10 +1,34 @@
 // script.js
 // by Preston Hager
 
-const defaultCode = '<body>\n<div class="section-orange">\n  <h1>A Div Demo</h1>\n  <div class="nested">\n    <p>You can nest divs inside of divs.</p>\n  </div>\n</div>\n</body>';
-const autoSaveTime = 1000 // time in miliseconds.
+// const defaultCode = '<body>\n<div class="orange">\n  <h1>A Div Demo</h1>\n  <div class="blue">\n    <p>You can nest divs inside of divs.</p>\n  </div>\n</div>\n</body>';
+const defaultCode = '<body>\n\n</body>';
+const codeAnimation = [
+  '<body>\n<div class="orange">\n  <h1>A Div Demo</h1>\n</div>\n</body>',
+  '<body>\n<div class="orange">\n  <h1>A Div Demo</h1>\n  <div class="blue">\n    <p>You can nest divs inside of divs.</p>\n  </div>\n</div>\n</body>'
+]
+const autoSaveTime = 3000 // time in miliseconds.
+var autoSaveTimeout = setTimeout(executeCode, autoSaveTime);
 
-const editorElement = document.querySelector("#editor");
+// create the code animation
+const animationLink = document.getElementById("animate");
+animationLink.addEventListener("click", function(e) {
+  var animation = genCode(codeAnimation);
+  function animationTimeout(code) {
+    setTimeout(() => {
+      var next = code.next();
+      if (! next.done) {
+        jar.updateCode(next.value);
+        executeCode();
+        animationTimeout(code);
+      }
+    }, 1000);
+  }
+  animationTimeout(animation)
+});
+
+// code for the editor
+const editorElement = document.getElementById("editor");
 // add the "Cmd+S" or "Ctrl+S" to execute the code.
 editorElement.addEventListener("keydown", function(e) {
   if ((e.ctrlKey || e.metaKey ) && e.keyCode === 83) {
@@ -22,9 +46,21 @@ var jar = CodeJar(editorElement, withLineNumbers(Prism.highlightElement, {
 });
 jar.updateCode(defaultCode);
 jar.onUpdate(code => {
-  cooldown = autoSaveTime;
+  clearTimeout(autoSaveTimeout);
+  autoSaveTimeout = setTimeout(executeCode, autoSaveTime);
 });
 
+var executeButton = document.getElementById("execute-button");
+var resetButton = document.getElementById("reset-button");
+executeButton.onclick = executeCode;
+resetButton.onclick = function (e) {
+  jar.updateCode(defaultCode);
+  executeCode();
+};
+
+executeCode();
+
+// HELPER FUNCTIONS
 function executeCode(e) {
   var frame = document.getElementById("explore-show");
   var frameDoc =
@@ -39,13 +75,8 @@ function executeCode(e) {
   );
   frameDoc.document.close();
 }
-
-var executeButton = document.getElementById("execute-button");
-var resetButton = document.getElementById("reset-button");
-executeButton.onclick = executeCode;
-resetButton.onclick = function (e) {
-  jar.updateCode(defaultCode);
-  executeCode();
-};
-
-executeCode();
+function* genCode(code) {
+  for (var c of code) {
+    yield c;
+  }
+}
