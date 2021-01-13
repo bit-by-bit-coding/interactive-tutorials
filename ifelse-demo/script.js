c0a57e7231f4d03968e86673e25cf49fb0b2f0b1
pjkpjk900 <pk58@buffalo.edu>
2021-01-13 14:14:42 -0500

    Added input boxes but javascript issue with operators remains

--- ifelse-demo/script.js
+++ ifelse-demo/script.js
@@ -1,7 +1,45 @@
-let drop = document.getElementById("dropdown");
-createOption(drop, {
-  "Small": "small",
-  "Medium": "medium",
-  "Large": "large",
-  "Work": "Please"
-}, optionHandler);
+let num1 = document.getElementById("num1");
+createOption(num1, {
+  0:0, 
+  1:1, 
+  2:2, 
+  3:3, 
+  4:4, 
+  5:5, 
+  6:6, 
+  7:7,
+  8:8, 
+  9:9
+});
+
+let num2 = document.getElementById("num2");
+createOption(num2, {
+  0:0, 
+  1:1, 
+  2:2, 
+  3:3, 
+  4:4, 
+  5:5, 
+  6:6, 
+  7:7,
+  8:8, 
+  9:9
+});
+
+let op = document.getElementById("op");
+createOption(op, {
+  "<":0,
+  ">":1, 
+  "=":2
+});
+
+document.getElementById("run").onclick = calculate;
+function calculate(){
+  console.log("Hey I'm running");
+  switch(op){
+    case 0:
+      if(num1 < num2) alert("True");
+      else alert("False")
+      break;
+  }
+}
