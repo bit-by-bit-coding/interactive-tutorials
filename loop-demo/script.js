0895a8beaae47523caf5f44dd6cd0be766061675
Emily Rose Baier <emilyrbaier@Emilys-MBP.hsd1.or.comcast.net>
2021-01-14 00:04:31 -0800

    added preliminary caterpillar divs, bitbybot head, functions to add sections

--- loop-demo/script.js
+++ loop-demo/script.js
@@ -1,31 +1,50 @@
 // script.js
 
-let congaCount = 1;
+let bodySegmentCount = 1;
 const minusButton = document.getElementById('minus-button')
 const plusButton = document.getElementById('plus-button')
+const caterpillar = document.getElementById('showcase-section')
 
-plusButton.addEventListener('click', onPlusClick);
-minusButton.addEventListener('click', onMinusClick);
+
 
 function onPlusClick() {
-    congaCount += 1;
-    document.getElementById('conga-line-count').innerHTML = congaCount;
-    console.log(congaCount)
+  bodySegmentCount += 1;
+    document.getElementById('min-max-message').innerHTML = ''
+    document.getElementById('body-segment-count').innerHTML = bodySegmentCount;
+    const bodySegment = document.createElement('div');
+    caterpillar.appendChild(bodySegment);
+    bodySegment.classList.add('body-segment');
+  //TODO: look at amount of children in caterpillar and limit to only 8 divs
+      if (bodySegmentCount > 8 ) {
+        bodySegmentCount= 8
+        document.getElementById('body-segment-count').innerHTML = bodySegmentCount;
+        document.getElementById('min-max-message').innerHTML = 'Only 8 body segments will fit!'
+      } else {
+        document.getElementById('conga-line-count').innerHTML = bodySegmentCount;
+      }
 };
 
 function onMinusClick() {
-  congaCount -= 1;
-  document.getElementById('conga-line-count').innerHTML = congaCount;
-  console.log(congaCount)
+  bodySegmentCount -= 1;
+  document.getElementById('min-max-message').innerHTML = ''
+  if (bodySegmentCount < 1 ) {
+    bodySegmentCount = 1
+    document.getElementById('body-segment-count').innerHTML = bodySegmentCount;
+    document.getElementById('min-max-message').innerHTML = 'You have to have at least 1 body segment!'
+  } else {
+    document.getElementById('conga-line-count').innerHTML = bodySegmentCount;
+  }
+  
+  console.log(bodySegmentCount)
 }
 
 
 
-// let executeCode = function (e) {
-//   updateConga(congaCount);
-// };
-// let autoSaveTime = 3000;
-// let autoSaveTimeout = setTimeout(executeCode, autoSaveTime);
+let executeCode = function (e) {
+  updateCongaLine(bodySegmentCount);
+};
+let autoSaveTime = 3000;
+let autoSaveTimeout = setTimeout(executeCode, autoSaveTime);
 
 let editorElement = document.getElementById("editor");
 // Add the "Cmd+S" or "Ctrl+S" to execute the code
@@ -43,7 +62,7 @@ let jar = CodeJar(editorElement, withLineNumbers(Prism.highlightElement, {
 }), {
   tab: '  '
 });
-jar.updateCode("let bitByBotCount = " + JSON.stringify(congaCount, null, '  '));
+jar.updateCode("let bodySegmentCount = " + bodySegmentCount);
 jar.onUpdate(code => {
   clearTimeout(autoSaveTimeout);
   autoSaveTimeout = setTimeout(executeCode, autoSaveTime);
@@ -56,14 +75,25 @@ jar.onUpdate(code => {
 
 /* update functions */
 
-let updateCongaLine = () => {
+let updateCaterpillar = (bodySegmentCount) => {
+
 };
 
 
 
 /* Add event listeners to buttons */
 
+document.getElementById("execute-button").addEventListener('click', (e) => {
+  executeCode()
+});
+document.getElementById("reset-button").addEventListener('click', (e) => {
+  bodySegmentCount = 1
+   jar.updateCode("let bodySegmentCount = " + bodySegmentCount);
 
-/* Create the options and update the car SVG */
+});
+
+plusButton.addEventListener('click', onPlusClick);
+minusButton.addEventListener('click', onMinusClick);
+
 
 
