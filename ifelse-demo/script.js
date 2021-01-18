f9016b445436e494a9705ec53fc04345ac5479e2
pjkpjk900 <pk58@buffalo.edu>
2021-01-18 12:26:32 -0500

    Working implementation ready

--- ifelse-demo/script.js
+++ ifelse-demo/script.js
@@ -1,4 +1,4 @@
-
+let resultBox = document.getElementById("result");
 
 let op = document.getElementById("op");
 createOption(op, {
@@ -15,26 +15,39 @@ function calculate(){
 
   if(op.value == '&lt;'){
     if(num1 < num2){
-      alert("True!");
+      resultBox.style.backgroundColor = "green";
+      resultBox.innerText = "True!";
     }else{
-      //Add explanation as to why it's wrong in the real demo box.
-      alert("False!");
+      resultBox.style.backgroundColor = "red";
+      if(num1 > num2){
+        resultBox.innerText = `This is false because number 1:${num1} is greater than number 2:${num2}.`;
+      }else if(num1 == num2){
+        resultBox.innerText = `This is false because number 1:${num1} is equal to number 2:${num2}, and not smaller than it.`;
+      }
     }
   }
 
   if(op.value == '='){
-    if(num1 == num2){
-      alert("True!");
+    if(num1 == num2){     
+      resultBox.style.backgroundColor = "green";
+      resultBox.innerText = "True!";
     }else{
-      alert("False!");
+      
+      resultBox.style.backgroundColor = "red";
+      resultBox.innerText = `This is false because number 1: ${num1} is not equal to number 2: ${num2}.`;
     }
   }
 
   if(op.value == '&gt;'){
     if(num1 > num2){
-      alert("True");
+      resultBox.style.backgroundColor = "green";
+      resultBox.innerText = "True!";
     }else{
-      alert("False");
+      if(num1 < num2){
+        resultBox.innerText = `This is false because number 1: ${num1} is smaller than number 2: ${num2}.`;
+      }else if(num1 == num2){
+        resultBox.innerText = `This is false because number 1: ${num1} is equal to number 2: ${num2}, and not strictly greater than it.`;
+      }
     }
   }
 }
