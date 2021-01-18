d38fd5178df23fca920ca77398b978cb4adb61e2
pjkpjk900 <pk58@buffalo.edu>
2021-01-18 03:55:33 -0500

    Got the operators to work

--- ifelse-demo/script.js
+++ ifelse-demo/script.js
@@ -1,45 +1,41 @@
-let num1 = document.getElementById("num1");
-createOption(num1, {
-  0:0, 
-  1:1, 
-  2:2, 
-  3:3, 
-  4:4, 
-  5:5, 
-  6:6, 
-  7:7,
-  8:8, 
-  9:9
-});
 
-let num2 = document.getElementById("num2");
-createOption(num2, {
-  0:0, 
-  1:1, 
-  2:2, 
-  3:3, 
-  4:4, 
-  5:5, 
-  6:6, 
-  7:7,
-  8:8, 
-  9:9
-});
 
 let op = document.getElementById("op");
 createOption(op, {
-  "<":0,
-  ">":1, 
-  "=":2
+  '<':'<', 
+  '>':'>', 
+  '=':'='
 });
 
-document.getElementById("run").onclick = calculate;
+
 function calculate(){
-  console.log("Hey I'm running");
-  switch(op){
-    case 0:
-      if(num1 < num2) alert("True");
-      else alert("False")
-      break;
+  let num1 = document.getElementById("num1").value;
+  let num2 = document.getElementById("num2").value;
+
+
+  if(op.value == '&lt;'){
+    if(num1 < num2){
+      alert("True!");
+    }else{
+      //Add explanation as to why it's wrong in the real demo box.
+      alert("False!");
+    }
+  }
+
+  if(op.value == '='){
+    if(num1 == num2){
+      alert("True!");
+    }else{
+      alert("False!");
+    }
+  }
+
+  if(op.value == '&gt;'){
+    if(num1 > num2){
+      alert("True");
+    }else{
+      alert("False");
+    }
   }
 }
+ 
