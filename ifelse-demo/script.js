3d44bed7a4df21921601cfd109a6c1a8e3559f3a
pjkpjk900 <pk58@buffalo.edu>
2021-01-19 00:26:15 -0500

    Finished up demo

--- ifelse-demo/script.js
+++ ifelse-demo/script.js
@@ -4,7 +4,9 @@ let op = document.getElementById("op");
 createOption(op, {
   '<':'<', 
   '>':'>', 
-  '=':'='
+  '===':'===',
+  '<=':'<=',
+  '>=':'>='
 });
 
 
@@ -13,7 +15,7 @@ function calculate(){
   let num2 = document.getElementById("num2").value;
 
 
-  if(op.value == '&lt;'){
+  if(op.value == '<'){
     if(num1 < num2){
       resultBox.style.backgroundColor = "green";
       resultBox.innerText = "True!";
@@ -27,27 +29,47 @@ function calculate(){
     }
   }
 
-  if(op.value == '='){
+  if(op.value == '==='){
     if(num1 == num2){     
       resultBox.style.backgroundColor = "green";
       resultBox.innerText = "True!";
     }else{
-      
       resultBox.style.backgroundColor = "red";
       resultBox.innerText = `This is false because number 1: ${num1} is not equal to number 2: ${num2}.`;
     }
   }
 
-  if(op.value == '&gt;'){
+  if(op.value == '>'){
     if(num1 > num2){
       resultBox.style.backgroundColor = "green";
       resultBox.innerText = "True!";
     }else{
+      resultBox.style.backgroundColor = "red";
       if(num1 < num2){
         resultBox.innerText = `This is false because number 1: ${num1} is smaller than number 2: ${num2}.`;
       }else if(num1 == num2){
         resultBox.innerText = `This is false because number 1: ${num1} is equal to number 2: ${num2}, and not strictly greater than it.`;
       }
+    }
+  }
+
+  if(op.value == '<='){
+    if(num1 <= num2){     
+      resultBox.style.backgroundColor = "green";
+      resultBox.innerText = "True!";
+    }else{
+      resultBox.style.backgroundColor = "red";
+      resultBox.innerText = `This is false because number 1: ${num1} is greater than number 2: ${num2}.`;
+    }
+  }
+
+  if(op.value == '>='){
+    if(num1 >= num2){     
+      resultBox.style.backgroundColor = "green";
+      resultBox.innerText = "True!";
+    }else{
+      resultBox.style.backgroundColor = "red";
+      resultBox.innerText = `This is false because number 1: ${num1} is smaller than number 2: ${num2}.`;
     }
   }
 }
