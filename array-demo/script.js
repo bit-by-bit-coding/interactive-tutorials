d2cc0dc622ae9ae046afd548ff1e728cc51b5a4e
Veronica Stork <zealous.anemone@gmail.com>
2021-01-28 16:49:46 -0500

    Changed from codejar to basic text area

--- array-demo/script.js
+++ array-demo/script.js
@@ -1,15 +1,23 @@
-let defaultCode = 'const arr = [1, 2, 3, 4, 5];';
+let arrayValues = document.getElementById('array-vals');
+let executeButton = document.getElementById('execute-button');
 
-let jar = CodeJar(
-  document.getElementById('editor'),
-  withLineNumbers(Prism.highlightElement, {
-    color: '#000',
-    backgroundColor: 'rgb(232, 232, 232)',
-  }),
-  {
-    tab: '  ',
-    indentOn: /<[a-zA-Z =\"\-]+>$/,
-  }
-);
+executeButton.onclick = () => {
+  showArray(arrayValues.value);
+};
 
-jar.updateCode(defaultCode);
+const showArray = (arr) => {
+  alert('Your array is [' + arr + ']');
+};
+// let jar = CodeJar(
+//   document.getElementById('editor'),
+//   withLineNumbers(Prism.highlightElement, {
+//     color: '#000',
+//     backgroundColor: 'rgb(232, 232, 232)',
+//   }),
+//   {
+//     tab: '  ',
+//     indentOn: /<[a-zA-Z =\"\-]+>$/,
+//   }
+// );
+
+// jar.updateCode(defaultCode);
