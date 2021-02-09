9510ac343fc5955d5f953bee01c8bece8041ca62
Veronica Stork <zealous.anemone@gmail.com>
2021-02-08 22:30:21 -0500

    Selected array element is highlighted in yellow.

--- array-demo/script.js
+++ array-demo/script.js
@@ -6,12 +6,13 @@ let arrayAtI = document.getElementById('array-at-i');
 let visualization = document.getElementById('visualization');
 
 // To do:
-// 1. Separate array slot #s visually
+// 1. Separate array slot #s visually DONE
 // 2. Explanation - note that arrays start at 0
 // 3. Fix tooltip
 // 4. Add text to tooltips
 // 5. use style to connect numbers and stuff more?
 // 6. Make clear button
+// 7. Make selected array element light up DONE
 
 arrayValues.defaultValue = '1, 2, 3, 4, 5';
 
@@ -37,6 +38,10 @@ const showVisualization = (arr) => {
     let textNode = document.createTextNode(arr[i]);
     node.setAttribute('class', 'array-element');
     node.setAttribute('id', i);
+    // Set 0 as default "selected" spot in array
+    if (i === 0) {
+      node.classList.add('selected');
+    }
     node.appendChild(iNode);
     node.appendChild(textNode);
     visualization.appendChild(node);
@@ -46,6 +51,13 @@ const showVisualization = (arr) => {
   document.addEventListener('click', function (e) {
     if (e.target && Number.isInteger(parseInt(e.target.id))) {
       let i = e.target.id;
+      // remove selected class from previously chosen array el
+      document
+        .getElementsByClassName('selected')[0]
+        .classList.remove('selected');
+      // add selected class to element you're clicking
+      document.getElementById(i).classList.add('selected');
+      // Textual representation of how contents of array shown at particular index
       arrayAtI.innerHTML = `myArray[${i}] = ${arr[i]}`;
     }
   });
@@ -55,4 +67,5 @@ const showArrayCode = (arr) => {
   // Display code to create array, length, & vals at different spots
   codeDisplay.innerHTML = `let myArray = [${arr}];`;
   arrayLength.innerHTML = `myArray.length = ${arr.length}`;
+  arrayAtI.innerHTML = `myArray[0] = ${arr[0]}`;
 };
