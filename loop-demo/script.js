56c5aa18a10275984668bf7d5f076f102ccf81bd
Emily Baier <hello@emilybaier.com>
2021-02-11 23:08:02 -0800

    we have bouncing!

--- loop-demo/script.js
+++ loop-demo/script.js
@@ -8,6 +8,8 @@ const plusButton = document.getElementById('plus-button');
 const caterpillar = document.getElementById('showcase-section');
 const blueButton = document.getElementById('blue-button');
 const orangeButton = document.getElementById('orange-button');
+const pinkButton = document.getElementById('pink-button');
+const bounceButton = document.getElementById('bounce-button');
 
 
 function addBodySegments(){
@@ -52,7 +54,7 @@ function onMinusClick() {
 function makeColorSegments(color) {
 const addedDivs = document.querySelectorAll('.body-segment');
 let running = 0;
-  if(running > 0) return;
+  if (running > 0) return;
 for (let i = 0; i < addedDivs.length; i++) {
   running++;
   setTimeout(function() {
@@ -63,8 +65,36 @@ for (let i = 0; i < addedDivs.length; i++) {
 }
 };
 
+let bouncing = false;
+function bounceSegments(){
+  const addedDivs = document.getElementsByClassName('body-segment');
+  if (!bouncing) {
+    bounceButton.innerText = 'make them stop!'
+    bouncing = true;
+    for (let i = 0; i < addedDivs.length; i++) {
+      setTimeout(function() {
+      addedDivs[i].classList.add('bounce');
+      }, 300*i);
+      
+    }
+  } else {
+    bounceButton.innerText= 'make them bounce!'
+    for (let i = 0; i < addedDivs.length; i++) {
+      setTimeout(function() {
+        addedDivs[i].classList.remove('bounce');
+      }, 300*i);
+    }
+    bouncing = false;
+      
+    }
+    
 
-blueButton.addEventListener('click', () => makeColorSegments('#38Bfe7'))
-orangeButton.addEventListener('click', () => makeColorSegments('#ff8900'))
+};
+
+
+blueButton.addEventListener('click', () => makeColorSegments('#05D1E2'));
+orangeButton.addEventListener('click', () => makeColorSegments('#ff8900'));
+pinkButton.addEventListener('click', () => makeColorSegments('#FD47B3'));
+bounceButton.addEventListener('click', () => bounceSegments());
 plusButton.addEventListener('click', onPlusClick);
 minusButton.addEventListener('click', onMinusClick);
