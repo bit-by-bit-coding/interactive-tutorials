124ee3136b3a60585c8975829c947ef535b59ac8
Emily Baier <hello@emilybaier.com>
2021-02-19 00:32:33 -0800

    formatted files

--- loop-demo/script.js
+++ loop-demo/script.js
@@ -11,16 +11,16 @@ const orangeButton = document.getElementById('orange-button');
 const pinkButton = document.getElementById('pink-button');
 const bounceButton = document.getElementById('bounce-button');
 const slowSpeed = document.getElementById('slow-speed');
-const showcaseSection = document.getElementById('showcase-section')
+const showcaseSection = document.getElementById('showcase-section');
 
 
-function addBodySegments(){
+function addBodySegments() {
   const bodySegment = document.createElement('div');
   const bodySegmentSpan = document.createElement('span');
   bodySegmentSpan.textContent = `${showcaseSection.children.length}`;
-  bodySegmentSpan.setAttribute('class','body-segment-number');
+  bodySegmentSpan.setAttribute('class', 'body-segment-number');
   bodySegment.appendChild(bodySegmentSpan);
-  if(bodySegmentCount < bodySegmentMaxLimit){
+  if (bodySegmentCount < bodySegmentMaxLimit) {
     bodySegmentCount++;
     document.getElementById('body-segment-count').innerHTML = bodySegmentCount;
     document.getElementById('min-max-message').innerHTML = '';
@@ -33,33 +33,33 @@ function addBodySegments(){
   }
 };
 
-function subtractBodySegments(){
-  if(bodySegmentCount > bodySegmentMinLimit){
+function subtractBodySegments() {
+  if (bodySegmentCount > bodySegmentMinLimit) {
     bodySegmentCount--;
     document.getElementById('body-segment-count').innerHTML = bodySegmentCount;
     document.getElementById('min-max-message').innerHTML = '';
-    let bodySegmentToRemove = document.querySelector(".body-segment:last-of-type")
+    let bodySegmentToRemove = document.querySelector(".body-segment:last-of-type");
     caterpillar.removeChild(bodySegmentToRemove);
-    
+
   } else if (bodySegmentCount <= bodySegmentMinLimit) {
     document.getElementById('body-segment-count').innerHTML = bodySegmentCount;
-    document.getElementById('min-max-message').innerHTML = 'You have to have at least 1!'
+    document.getElementById('min-max-message').innerHTML = 'You have to have at least 1!';
     return;
   }
 };
 
 function onPlusClick() {
-    addBodySegments();
+  addBodySegments();
 };
 
 function onMinusClick() {
   subtractBodySegments();
-}
+};
 
 
 
 let bouncing = false;
-function bounceSegments(){
+function bounceSegments() {
   let addedDivs = document.getElementsByClassName('body-segment');
   if (!bouncing) {
     bounceButton.innerText = 'reset bounce';
@@ -67,43 +67,43 @@ function bounceSegments(){
     document.getElementById('code-description').innerHTML = 'bodySegments[i].classList.add(\'bounce\');';
     bouncing = true;
     for (let i = 0; i < addedDivs.length; i++) {
-      if(slowSpeed.checked) {
-        setTimeout(function() {
+      if (slowSpeed.checked) {
+        setTimeout(function () {
           addedDivs[i].classList.add('bounce');
-          }, 300*i);
+        }, 300 * i);
       } else {
         addedDivs[i].classList.add('bounce');
       }
     }
   } else {
-    bounceButton.innerText= 'make them bounce!';
+    bounceButton.innerText = 'make them bounce!';
     bouncing = false;
     document.getElementById('change-description').innerHTML = '';
     document.getElementById('code-description').innerHTML = '';
     for (let i = 0; i < addedDivs.length; i++) {
-        addedDivs[i].classList.remove('bounce');
+      addedDivs[i].classList.remove('bounce');
     }
   }
 };
 
 function makeColorSegments(color) {
   let addedDivs = document.querySelectorAll('.body-segment');
-  if(bouncing) {
-    bounceButton.innerText= 'make them bounce!';
+  if (bouncing) {
+    bounceButton.innerText = 'make them bounce!';
     bouncing = false;
     for (let i = 0; i < addedDivs.length; i++) {
-        addedDivs[i].classList.remove('bounce');
+      addedDivs[i].classList.remove('bounce');
     }
   }
   for (let i = 0; i < addedDivs.length; i++) {
-    if(slowSpeed.checked) {
-      setTimeout(function() {
+    if (slowSpeed.checked) {
+      setTimeout(function () {
         addedDivs[i].style.backgroundColor = color;
-      }, 300*i);
+      }, 300 * i);
     } else {
       addedDivs[i].style.backgroundColor = color;
-    };
-  };
+    }
+  }
 };
 
 function onBlueButtonClick() {
