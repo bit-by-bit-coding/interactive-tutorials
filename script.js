f9f79c2b535e38cbaa4b69b6274ef4b3e52e970b
praveer35 <51690132+praveer35@users.noreply.github.com>
2021-06-27 13:44:00 -0700

    ordered demos from least to most advanced

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 // Edit this object when adding a new Interactive Tutorials!
 
 let interactiveTutorials = {
-  divDemo: {
-    path: 'div-demo',
-    title: '&ltDIV&gt TAG DEMO',
-    content: "How to use the &ltdiv&gt tag and an exploritory tutorial."
-  },
   attributeDemo: {
     path: 'attribute-demo',
     title: 'ATTRIBUTE DEMO',
     content: "Learn how attributes work inside tags and the most commonly used ones."
+  },
+  divDemo: {
+    path: 'div-demo',
+    title: '&ltDIV&gt TAG DEMO',
+    content: "How to use the &ltdiv&gt tag and an exploritory tutorial."
   },
   boxModel: {
     path: 'box-model-demo',
@@ -31,27 +31,27 @@ let interactiveTutorials = {
     title: 'FUNCTION DEMO',
     content: "Condensing code and calling upon functions!"
   },
-  objectDemo: {
-    path: 'object-demo',
-    title: 'OBJECT DEMO',
-    content: "What are objects in JavaScript? This tutorial shows the syntax of a JSON object with a neat car that changes color!"
-  },
   loopDemo: {
     path: 'for-loop-demo',
     title: 'FOR LOOP DEMO',
     content: "Explore how the for loop works with a bot-a-pillar!"
+  },
+  mathRandom: {
+    path: 'math-random-demo',
+    title: 'MATH.RANDOM FUNCTION DEMO',
+    content: "How does the Math.random function work? How can it be modified to fit specific needs?"
   },
   eventsDemo: {
     path: 'events-demo',
     title: 'EVENTS DEMO',
     content: "What are events in JavaScript, and how can we use them?"
   },
-  mathRandom: {
-    path: 'math-random-demo',
-    title: 'MATH.RANDOM FUNCTION DEMO',
-    content: "How does the Math.random function work? How can it be modified to fit specific needs?"
+  objectDemo: {
+    path: 'object-demo',
+    title: 'OBJECT DEMO',
+    content: "What are objects in JavaScript? This tutorial shows the syntax of a JSON object with a neat car that changes color!"
   }
-}
+};
 
 // And that's it! It is added now!
 
