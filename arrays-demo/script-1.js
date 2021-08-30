9cbdfc382f084e6d1b8b140de19c28999d09e247
alonr619 <alan.ragoler@gmail.com>
2021-08-29 17:01:41 -0700

    Added code functionality to first interactive

--- arrays-demo/script-1.js
+++ arrays-demo/script-1.js
@@ -1,8 +1,12 @@
-const l = ["dog", "cat", "mouse"];
+let l = ["dog", "cat", "mouse"];
 let pushInput = document.getElementById("push-input-1");
 let pushButton = document.getElementById("push-button-1");
 let pArray = document.getElementById("p-array-1");
 let pError = document.getElementById('p-error-1');
+let pCode = document.getElementById('p-code-1');
+let inputCode = document.getElementById('input-code-1');
+let clearButton = document.getElementById('clear-button-1');
+let buttonCode = document.getElementById('button-code-1');
 
 pushButton.onclick = function() {
     pError.innerText = '';
@@ -13,6 +17,7 @@ pushButton.onclick = function() {
     } else {
         l.push(pushInput.value);
         pArray.innerText = "Array: [" + l.join(", ") + "]";
+        pCode.innerText = pCode.innerText + '\n' + 'animals.push("' + pushInput.value + '");'
     }
 };
 
@@ -22,3 +27,47 @@ pushInput.addEventListener("keyup", function(event) {
         pushButton.click();
     }
 });
+
+buttonCode.onclick = function() {
+    let x = inputCode.value;
+    if (!(x.indexOf('animals') === 0)) {
+        if ((x.toLowerCase().indexOf('animal.') === 0) || (x.toLowerCase().indexOf('animals.') === 0)) {
+            pError.innerText = 'Remember that the variable name needs to be exactly the same as what it was defined at the start';
+        } else {
+            pError.innerText = 'Remember that the push command comes in the form of animals.push("string");';
+        }
+    } else if (!(x.indexOf('animals.push("') === 0)) {
+        pError.innerText = 'Don\'t forget that the method we use to add elements to a list is "push"! You also need to open parantheses and have double quotes around the new string.';
+    } else if (!(x.substr(x.length - 1, 1) === ';')) {
+        pError.innerText = 'Remember that Javascript commands need to end with a semicolon (;)!';
+    } else if (!(x.substr(x.length - 2, 1) === ')')) {
+        pError.innerText = 'Remember that you need to close the parantheses!';
+    } else if (!(x.substr(x.length - 3, 1) === '"')) {
+        pError.innerText = "Remember that you need to end your double quotes!"
+    } else if ((x.match(/"/g) || []).length > 2) {
+        pError.innerText = 'You can\'t have a double quote in your string!';
+    }
+    else {
+        pError.innerText = '';
+        if (x.slice(14,x.length - 3).length > 50) {
+            pError.innerText = "Please limit your element length to 50.";
+            return
+        }
+        l.push(x.slice(14,x.length - 3));
+        pArray.innerText = "Array: [" + l.join(", ") + "]";
+        pCode.innerText = pCode.innerText + '\n' + x;
+    }
+};
+
+inputCode.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        buttonCode.click();
+    }
+});
+
+clearButton.onclick = function() {
+    l = []
+    pCode.innerText = 'let animals = [];'
+    pArray.innerText = 'Array: []'
+}
