077e8f2da82ffc930b8a4294dddac280fda2f8d6
alonr619 <alan.ragoler@gmail.com>
2021-09-03 19:50:39 -0700

    Added code box to second interactive

--- arrays-demo/script-2.js
+++ arrays-demo/script-2.js
@@ -1,41 +1,26 @@
 const l2 = ["dog", "cat", "mouse"];
-let pushInput2 = document.getElementById("push-input-2");
-let pushButton2 = document.getElementById("push-button-2");
 let indexInput2 = document.getElementById('index-input-2');
 let indexButton2 = document.getElementById("index-button-2");
-let indexGetP2 = document.getElementById("index-get-p-2");
 let pArray2 = document.getElementById("p-array-2");
 let pError2 = document.getElementById('p-error-2');
-
-pushButton2.onclick = function() {
-    pError2.innerText = '';
-    if (pushInput2.value === '') {
-        pError2.innerText = "The new element can't be nothing!";
-    } else if (pushInput2.value.length > 50) {
-        pError2.innerText = "Please limit your element length to 50.";
-    } else {
-        l2.push(pushInput2.value);
-        pArray2.innerText = "Array: [" + l2.join(", ") + "]";
-    }
-};
-
-pushInput2.addEventListener("keyup", function(event) {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        pushButton2.click();
-    }
-});
+let pCode2 = document.getElementById('p-code-2');
+let inputCode2 = document.getElementById('input-code-2');
+let clearButton2 = document.getElementById('clear-button-2');
+let buttonCode2 = document.getElementById('button-code-2');
+let consoleOutput2 = document.getElementById('console-output-2');
 
 indexButton2.onclick = function() {
-    i = indexInput2.value;
+    let i = indexInput2.value;
     if (i === '') {
-        indexGetP2.innerText = "The index can't be nothing!";
+        pError2.innerText = "The index can't be nothing!";
     } else if (i < 0) {
-        indexGetP2.innerText = "The index must be greater than or equal to 0.";
+        pError2.innerText = "The index must be greater than or equal to 0.";
     } else if (i >= l2.length) {
-        indexGetP2.innerText = "The index must be less than the length of the array. (remember that we start counting the index from 0!)";
+        pError2.innerText = "The index must be less than the length of the array. (remember that we start counting the index from 0!)";
     } else {
-        indexGetP2.innerText = "The element with index " + i.toString() + " is: " + l2[i.toString()];
+        consoleOutput2.innerText = consoleOutput2.innerText + '\n' + l2[i];
+        pError2.innerText = '';
+        pCode2.innerText = pCode2.innerText + '\n' + 'console.log(animals[' + i.toString() + ']);';
     }
 };
 
@@ -45,3 +30,51 @@ indexInput2.addEventListener("keyup", function(event) {
         indexButton2.click();
     }
 });
+
+buttonCode2.onclick = function() {
+    let x = inputCode2.value;
+    if (!(x.indexOf('console.log(animals[') === 0)) {
+        if ((x.toLowerCase().indexOf('animal') === 12) || (x.toLowerCase().indexOf('animals') === 12)) {
+            pError2.innerText = 'Remember that the variable name needs to be exactly the same as what it was defined at the start';
+        } else {
+            pError2.innerText = 'Remember that the console.log command comes in the form of console.log(animals[index]);';
+        }
+    } else if (!(x.substr(x.length - 1, 1) === ';')) {
+        pError2.innerText = 'Remember that Javascript commands need to end with a semicolon (;)!';
+    } else if (!(x.substr(x.length - 2, 1) === ')')) {
+        pError2.innerText = 'Remember that you need to close the parantheses!';
+    } else if (!(x.substr(x.length - 3, 1) === ']')) {
+        pError2.innerText = 'Remember that you need to close the brackets!';
+    } else {
+        try {
+            y = Number(x.slice(20, x.length - 3));
+            if (isNaN(y)) {
+                pError2.innerText = 'Remember that you need to pass in an integer between the brackets!';
+            } else if (y < 0) {
+                pError2.innerText = 'Remember that the integer can\'t be negative!';
+            } else if (y >= l2.length) {
+                pError2.innerText = 'Remember that the integer can\'t be greater than or equal to the length of the array!'
+            } else {
+                consoleOutput2.innerText = consoleOutput2.innerText + '\n' + l2[y];
+                pError2.innerText = '';
+                pCode2.innerText = pCode2.innerText + '\n' + x;
+            }
+        } catch {
+            pError2.innerText = 'Remember that you need to pass in an integer between the brackets!'
+        }
+    }
+};
+
+inputCode2.addEventListener("keyup", function(event) {
+    if (event.key === "Enter") {
+        event.preventDefault();
+        buttonCode2.click();
+    }
+});
+
+clearButton2.onclick = function() {
+    consoleOutput2.innerText = 'Console Output:';
+    pCode2.innerText = 'let animals = ["dog", "cat", "mouse"];';
+    indexGetP2.innerText = '';
+    pError2.innerText = '';
+}
