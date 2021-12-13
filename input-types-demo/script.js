743cd4e707ed8484a2639874da23d043cc8971cd
Shihua You <youshihuaaria@gmail.com>
2021-12-12 22:14:14 -0500

    Update script.js
    Modified code layout

--- input-types-demo/script.js
+++ input-types-demo/script.js
@@ -1,3 +1,80 @@
+/* Default Code */
+let textDefaultCode = `
+<form>\n 
+    <label for="fname">Enter your first name:</label><br>\n 
+    <input type="text" id="fname" name="fname"><br>\n 
+    <label for="lname">Enter your last name:</label><br>\n 
+    <input type="text" id="lname" name="lname">\n 
+</form>`;
+
+let radioDefaultCode = `
+<form>\n 
+    <p>Select your favorite Web language</p>\n 
+    <input type="radio" id="html" name="fav_language" value="HTML">\n 
+    <label for="html">HTML</label><br>\n 
+    <input type="radio" id="css" name="fav_language" value="CSS">\n 
+    <label for="css">CSS</label><br> \n 
+    <input type="radio" id="javascript" name="fav_language" value="JavaScript"> \n 
+    <label for="javascript">JavaScript</label> \n 
+</form>`;
+
+let checkboxDefaultCode = `
+<form> \n 
+    <p>Select Courses</p> \n 
+    <input type="checkbox" id="course1" name="course1" value="Art"> \n 
+    <label for="course1"> Art </label><br> \n 
+    <input type="checkbox" id="course2" name="course2" value="History"> \n 
+    <label for="course2"> History </label><br> \n 
+    <input type="checkbox" id="course3" name="course3" value="Math"> \n 
+    <label for="course3"> Math </label><br> \n 
+    <input type="checkbox" id="course4" name="course4" value="Science"> \n 
+    <label for="course4"> Science </label><br> \n 
+    <input type="checkbox" id="course5" name="course5" value="Physics"> \n 
+    <label for="course5"> Physics </label><br> \n 
+</form>`;
+
+let buttonDefaultCode = `<input type="button" onclick="alert('Hello World!')" value="Click Me!">`;
+
+let numberDefaultCode = `
+<form>\n
+    <p>Pick a number!</p>\n
+    <label for="quantity">Quantity (between 1 and 10):</label>\n
+    <input type="number" id="quantity" name="quantity" min="1" max="10"> \n
+</form>`;
+
+let colorDefaultCode = `
+<form>\n
+    <label for="favcolor">Select your favorite color:</label> \n
+    <input type="color" id="favcolor" name="favcolor">\n
+</form>`;
+
+let dateDefaultCode = `
+<form>\n
+    <label for="randomDate">Pick a random date:</label> \n
+    <input type="date" id="randomDate" name="randomDate">\n
+</form>`;
+
+let othersDefaultCode = `<form>\n
+    <label for="email">Enter your email:</label> \n
+    <input type="email" id="email">\n
+</form>\n
+<form>\n
+    <label for="myfile">Select a file:</label>\n
+    <input type="file" id="myfile" name="myfile">\n
+</form>\n
+<form>\n
+    <label for="vol">Select volume (between 0 and 100):</label>\n
+    <input type="range" id="vol" name="vol" min="0" max="100">\n
+</form>\n
+<form>\n
+    <label for="phone">Enter your phone number:</label>\n
+    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"> \n
+</form>\n
+<form>\n
+    <label for="appt">Select a time:</label>\n
+    <input type="time" id="appt" name="appt">\n
+</form>\n`;
+
 /* Text Editor */
 let TextJar = CodeJar(document.getElementById("text-input-editor"), withLineNumbers(Prism.highlightElement, {
     color: '#000',
@@ -7,13 +84,6 @@ let TextJar = CodeJar(document.getElementById("text-input-editor"), withLineNumb
     indentOn: /<[a-zA-Z =\"\-]+>$/
 });
 
-let textDefaultCode = `
-<form>\n 
-    <label for="fname">Enter your first name:</label><br>\n 
-    <input type="text" id="fname" name="fname"><br>\n 
-    <label for="lname">Enter your last name:</label><br>\n 
-    <input type="text" id="lname" name="lname">\n 
-</form>`;
 let textElementId = "text-input-frame";
 
 TextJar.updateCode(textDefaultCode);
@@ -37,17 +107,6 @@ let RadioJar = CodeJar(document.getElementById("radio-input-editor"), withLineNu
     tab: '  ',
     indentOn: /<[a-zA-Z =\"\-]+>$/
 });
-
-let radioDefaultCode = `
-<form>\n 
-    <p>Select your favorite Web language</p>\n 
-    <input type="radio" id="html" name="fav_language" value="HTML">\n 
-    <label for="html">HTML</label><br>\n 
-    <input type="radio" id="css" name="fav_language" value="CSS">\n 
-    <label for="css">CSS</label><br> \n 
-    <input type="radio" id="javascript" name="fav_language" value="JavaScript"> \n 
-    <label for="javascript">JavaScript</label> \n 
-</form>`;
 
 let radioElementId = "radio-input-frame";
 
@@ -73,20 +132,6 @@ let CheckboxJar = CodeJar(document.getElementById("checkbox-input-editor"), with
     indentOn: /<[a-zA-Z =\"\-]+>$/
 });
 
-let checkboxDefaultCode = `
-<form> \n 
-    <p>Select Courses</p> \n 
-    <input type="checkbox" id="course1" name="course1" value="Art"> \n 
-    <label for="course1"> Art </label><br> \n 
-    <input type="checkbox" id="course2" name="course2" value="History"> \n 
-    <label for="course2"> History </label><br> \n 
-    <input type="checkbox" id="course3" name="course3" value="Math"> \n 
-    <label for="course3"> Math </label><br> \n 
-    <input type="checkbox" id="course4" name="course4" value="Science"> \n 
-    <label for="course4"> Science </label><br> \n 
-    <input type="checkbox" id="course5" name="course5" value="Physics"> \n 
-    <label for="course5"> Physics </label><br> \n 
-</form>`;
 let checkboxElementId = "checkbox-input-frame";
 
 CheckboxJar.updateCode(checkboxDefaultCode);
@@ -111,7 +156,6 @@ let ButtonJar = CodeJar(document.getElementById("button-input-editor"), withLine
     indentOn: /<[a-zA-Z =\"\-]+>$/
 });
 
-let buttonDefaultCode = `<input type="button" onclick="alert('Hello World!')" value="Click Me!">`;
 let buttonElementId = "button-input-frame";
 
 ButtonJar.updateCode(buttonDefaultCode);
@@ -136,12 +180,6 @@ let NumberJar = CodeJar(document.getElementById("number-input-editor"), withLine
     indentOn: /<[a-zA-Z =\"\-]+>$/
 });
 
-let numberDefaultCode = `
-<form>\n
-    <p>Pick a number!</p>\n
-    <label for="quantity">Quantity (between 1 and 10):</label>\n
-    <input type="number" id="quantity" name="quantity" min="1" max="10"> \n
-</form>`;
 let numberElementId = "number-input-frame";
 
 NumberJar.updateCode(numberDefaultCode);
@@ -166,11 +204,6 @@ let ColorJar = CodeJar(document.getElementById("color-input-editor"), withLineNu
     indentOn: /<[a-zA-Z =\"\-]+>$/
 });
 
-let colorDefaultCode = `
-<form>\n
-    <label for="favcolor">Select your favorite color:</label> \n
-    <input type="color" id="favcolor" name="favcolor">\n
-</form>`;
 let colorElementId = "color-input-frame";
 
 ColorJar.updateCode(colorDefaultCode);
@@ -195,11 +228,6 @@ let DateJar = CodeJar(document.getElementById("date-input-editor"), withLineNumb
     indentOn: /<[a-zA-Z =\"\-]+>$/
 });
 
-let dateDefaultCode = `
-<form>\n
-    <label for="randomDate">Pick a random date:</label> \n
-    <input type="date" id="randomDate" name="randomDate">\n
-</form>`;
 let dateElementId = "date-input-frame";
 
 DateJar.updateCode(dateDefaultCode);
@@ -224,26 +252,6 @@ let jar = CodeJar(document.getElementById("others-input-editor"), withLineNumber
     indentOn: /<[a-zA-Z =\"\-]+>$/
 });
 
-let othersDefaultCode = `<form>\n
-    <label for="email">Enter your email:</label> \n
-    <input type="email" id="email">\n
-</form>\n
-<form>\n
-    <label for="myfile">Select a file:</label>\n
-    <input type="file" id="myfile" name="myfile">\n
-</form>\n
-<form>\n
-    <label for="vol">Select volume (between 0 and 100):</label>\n
-    <input type="range" id="vol" name="vol" min="0" max="100">\n
-</form>\n
-<form>\n
-    <label for="phone">Enter your phone number:</label>\n
-    <input type="tel" id="phone" name="phone" pattern="[0-9]{3}-[0-9]{2}-[0-9]{3}"> \n
-</form>\n
-<form>\n
-    <label for="appt">Select a time:</label>\n
-    <input type="time" id="appt" name="appt">\n
-</form>\n`;
 let othersElementId = "others-input-frame";
 
 jar.updateCode(othersDefaultCode);
